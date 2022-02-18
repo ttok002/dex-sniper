6126836c7729f0f15e6cc719fc1dd37bbf5c532e
coccoinomane <coccoinomane@gmail.com>
2022-02-18 09:56:36 +0100

    Fix factory address for TraderJoe

--- src/dexes/uniswapV2Clones/TraderJoe.ts
+++ src/dexes/uniswapV2Clones/TraderJoe.ts
@@ -3,7 +3,7 @@ import { UniswapV2Clone } from "./UniswapV2Clone";
 export class TraderJoe extends UniswapV2Clone {
   supportedNetworks: string[] = ["avalanche"];
   routerAddress = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d";
-  factoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+  factoryAddress = "0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10";
   routerAbi = require("./abi/traderJoe/router.json");
   pairAbi = require("./abi/traderJoe/pair.json");
   factoryAbi = require("./abi/traderJoe/factory.json");
