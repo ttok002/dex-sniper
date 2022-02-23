5176746b8c4185fe0c4040aaeb316b02cf837b86
coccoinomane <coccoinomane@gmail.com>
2022-02-23 11:26:07 +0100

    Add solhint files

--- bash/idos/TraderJoe/ISA-WAVAX.sh
+++ bash/idos/TraderJoe/ISA-WAVAX.sh
@@ -7,16 +7,15 @@
 ##
 
 nBlocks=${1-"3600"}
-mainToken=1
+mainToken=0
 dex="TraderJoe"
 network="avalanche"
 pair="0x9155f441ffdfa81b13e385bfac6b3825c05184ee"
-blockFirstSwap="9317978"
-blockFirstMint="9317978"
 outputDir="output/Avalanche/TraderJoe/ISA-WAVAX"
 
-# getSwaps
+# Get swaps and mints since pair creation
+blockFirstSwap="9242098"
+blockFirstMint="9242098"
 hh uniswapV2Clone:getSwaps ${dex} ${pair} --fromblock ${blockFirstSwap} --nblocks "${nBlocks}" --network ${network} --maintoken $mainToken --csv "${outputDir}/swaps-first-${nBlocks}_blocks.csv"
-
-# getMints
 hh uniswapV2Clone:getMints ${dex} ${pair} --fromblock ${blockFirstMint} --nblocks "${nBlocks}" --network ${network} --maintoken $mainToken --csv "${outputDir}/mints-first-${nBlocks}_blocks.csv"
+
