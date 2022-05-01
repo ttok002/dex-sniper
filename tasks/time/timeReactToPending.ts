0670b73ddf39d370ecda9a4ad86a2dc4ea3d880f
coccoinomane <coccoinomane@gmail.com>
2022-05-01 22:23:43 +0200

    Add summary to timeReactToPending.ts

--- tasks/time/timeReactToPending.ts
+++ tasks/time/timeReactToPending.ts
@@ -1,39 +1,47 @@
 import { task, types } from 'hardhat/config';
-import { exit } from 'process';
 import { wait } from '../../src/helpers/general';
 import { prettyPrint, printTxResponse } from '../../src/helpers/print';
 import { getProvider, getSigner } from '../../src/helpers/providers';
-import { isResponsePending, isResponseTo } from '../../src/helpers/transactions';
+import { isResponseFrom, isResponsePending } from '../../src/helpers/transactions';
+import { TxTracker } from '../../src/tracking/TxTracker';
 
 task(
   'time:reactToPending',
-  'Set up a listener that immediately reacts by self-sending 1 wei. Useful to time reaction speed.'
+  'If the given address sends a transaction, react by self-sending 1 wei. Useful to time reaction speed.'
 )
-  .addOptionalParam('nMax', 'Max number of txs to listen to', 1, types.int)
+  .addOptionalParam('from', 'Address to monitor')
+  .addOptionalParam(
+    'nMax',
+    'Stop listening after nMax transactions (to avoid infinite loops)',
+    1,
+    types.int
+  )
   .addOptionalParam(
     'trigger',
-    'Immediately trigger the listener by sending 1 wei. This is the fastest possible scenario because trigger & listener are on the same node.',
+    'Self-send 1 wei immediately after setting up the listener. If the address is the same as the account, the reactiom will be automatically triggered. this is the fastest possible scenario because trigger & listener are on the same node.',
     false,
     types.boolean
   )
-  .setAction(async ({ nMax, trigger }, hre) => {
+  .setAction(async ({ from, nMax, trigger }, hre) => {
     const t0 = Date.now();
+    const txTracker = new TxTracker();
     let n = 0;
     // Get signer
     const provider = getProvider(hre);
     const signer = getSigner(hre, provider);
-    const address = await signer.getAddress();
+    const self = await signer.getAddress();
     // Listen to tx
     provider.on('pending', async (txHash) => {
       // Consider only txs from address
       const txRes = await provider.getTransaction(txHash);
-      if (!isResponseTo(txRes, address)) {
+      if (!isResponseFrom(txRes, from)) {
         return;
       }
       // React only to pending transactions
       if (!isResponsePending(txRes)) {
         return;
       }
+      txTracker.add(txRes.hash, ['in']);
       printTxResponse(txRes, 'Received tx!', [
         ['time', Date.now() - t0],
         ['iteration', n],
@@ -47,23 +55,34 @@ task(
           ['iteration', n],
         ]);
         provider.removeAllListeners();
-        return;
       }
       // React immediately by sending 1 gwei
       prettyPrint(`Reacting to ${txRes.hash.substring(0, 7)}...`, [
         ['time', Date.now() - t0],
         ['iteration', n],
       ]);
-      const sendTxRes = await signer.sendTransaction({ to: address, value: 1 });
+      const sendTxRes = await signer.sendTransaction({ to: self, value: 1 });
+      txTracker.add(sendTxRes.hash, ['out', `triggered by ${txRes.hash.substring(0, 7)}`]);
       printTxResponse(sendTxRes, `Reaction to ${txRes.hash.substring(0, 7)}`, [
         ['time', Date.now() - t0],
         ['iteration', n],
       ]);
+      // Wrap up!
+      if (n === nMax) {
+        // Print block numbers
+        const loggedTxs = await txTracker.fetchReceipts(provider);
+        prettyPrint(
+          'Transactions summary',
+          loggedTxs.map((tx) => [tx.hash, `${tx.blockNumber} [${tx.tags}]`])
+        );
+        return;
+      }
     });
     // Optionally trigger the listener
     if (trigger) {
       prettyPrint('Sending trigger...', [['time', Date.now() - t0]]);
-      const sendTxRes = await signer.sendTransaction({ to: address, value: 1 });
+      const sendTxRes = await signer.sendTransaction({ to: self, value: 1 });
+      txTracker.add(sendTxRes.hash, ['out', 'trigger']);
       printTxResponse(sendTxRes, 'Trigger response', [['time', Date.now() - t0]]);
     }
     return wait();
