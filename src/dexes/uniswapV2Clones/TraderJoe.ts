6523eb2a95cad266a8224edb441fc48b21739111
coccoinomane <coccoinomane@gmail.com>
2022-05-01 13:27:00 +0200

    Enabled TraderJoe on Avalanche Validator

--- src/dexes/uniswapV2Clones/TraderJoe.ts
+++ src/dexes/uniswapV2Clones/TraderJoe.ts
@@ -1,7 +1,7 @@
 import { UniswapV2Clone } from './UniswapV2Clone';
 
 export class TraderJoe extends UniswapV2Clone {
-  supportedNetworks: string[] = ['avalanche'];
+  supportedNetworks: string[] = ['avalanche', 'avalancheValidator'];
   routerAddress = '0x60aE616a2155Ee3d9A68541Ba4544862310933d4';
   factoryAddress = '0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10';
   routerAbi = require('./abi/traderJoe/router.json');
