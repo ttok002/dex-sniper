cbbbb8fa58ccae2271b316e3c88a22224dd009f3
coccoinomane <coccoinomane@gmail.com>
2022-05-06 18:21:40 +0200

    Add repeat support to sendEth task

--- tasks/utils/sendEth.ts
+++ tasks/utils/sendEth.ts
@@ -1,4 +1,5 @@
 import { task, types } from 'hardhat/config';
+import { sleep } from '../../src/helpers/general';
 import { prepare, prettyPrint, printTxReceipt } from '../../src/helpers/print';
 import { getSigner } from '../../src/helpers/signers';
 
@@ -6,7 +7,9 @@ task('utils:sendEth', 'Send some ETH to the give address')
   .addOptionalPositionalParam('to', 'Recipient address; default is self.')
   .addOptionalPositionalParam('valueInEth', 'Amount to send in ETH', '0.000000001', types.string)
   .addParam('account', "Who's sending the monies")
-  .setAction(async ({ to, valueInEth, account }, hre) => {
+  .addOptionalParam('n', 'How many times to send ETH', 1, types.int)
+  .addOptionalParam('delay', 'Delay in milliseconds between each send', 0, types.int)
+  .setAction(async ({ to, valueInEth, account, n, delay }, hre) => {
     if (!to) {
       to = await getSigner(hre, account).getAddress();
     }
@@ -15,7 +18,16 @@ task('utils:sendEth', 'Send some ETH to the give address')
       value: hre.ethers.utils.parseEther(valueInEth),
     };
     prettyPrint('Params', prepare(params));
-    const txRes = await getSigner(hre, account).sendTransaction(params);
-    const txReceipt = await txRes.wait();
-    printTxReceipt(txReceipt);
+    return new Promise(async (resolve) => {
+      for (let i = 0; i < n; i++) {
+        const txRes = await getSigner(hre, account).sendTransaction(params);
+        if (delay && i > 0) {
+          prettyPrint(`Waiting ${delay} ms...`);
+          await sleep(delay);
+        }
+        const txReceipt = await txRes.wait();
+        printTxReceipt(txReceipt, undefined, undefined, n > 1 ? [['n', i + 1]] : undefined);
+      }
+      resolve(n);
+    });
   });
