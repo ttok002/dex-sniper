ba4f37ba2eccbde89cc21ca045f3261c5a7f9d84
coccoinomane <coccoinomane@gmail.com>
2022-05-07 13:08:59 +0200

    SCRIPT_PATH constant + check .env exists

--- src/common/dotenv.ts
+++ src/common/dotenv.ts
@@ -1,7 +1,27 @@
 import * as dotenv from 'dotenv';
+import { existsSync } from 'fs';
+import { resolve } from 'path';
 
-dotenv.config();
+/**
+ * Directory from which the script was launched
+ */
+export const SCRIPT_PATH = process.cwd();
 
+/**
+ * Path where the .env file should be
+ */
+export const DOT_ENV_PATH = resolve(SCRIPT_PATH, '.env');
+
+if (!existsSync(DOT_ENV_PATH)) {
+  throw new Error(`Could not find .env file, are you in the project's root dir?`);
+}
+
+dotenv.config({ path: DOT_ENV_PATH });
+
+/**
+ * Get variable from the environment with optional
+ * fallback
+ */
 export function getenv(key: string, fallback: string = ''): string {
   return process.env[key] ?? fallback;
 }
